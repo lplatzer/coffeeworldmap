30da93eb375de47ddec1af20d40797ecc2cd2fba
Luis <dev@luisplatzer.de>
2024-01-25 11:12:06 +0100

    removed login button on logged in user state

--- src/app/components/userHeader.tsx
+++ src/app/components/userHeader.tsx
@@ -21,9 +21,11 @@ export default function UserHeader() {
             <div className="absolute left-1/2 z-10 mt-5 flex w-screen max-w-max -translate-x-1/2 px-4">
                 <div className="w-screen max-w-md flex-auto overflow-hidden rounded-3xl bg-white text-sm leading-6 shadow-lg ring-1 ring-gray-900/5">
                     <div className="p-4">
-                        <a href="api/auth/login" className="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-200 text-center grow rounded-lg">
-                            Login
-                        </a>
+                        {!user && (
+                            <a href="api/auth/login" className="text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-200 text-center grow rounded-lg">
+                                Login
+                            </a>
+                        )}
                         {user && (
                             <div>
                                 <p>{user.name}</p>
